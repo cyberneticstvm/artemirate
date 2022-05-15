e4fc35c621ca925a343b01ece1b0059e39fdb538
vijoy sasidharan <devuinfosys@gmail.com>
2022-05-15 10:01:19 +0530

    Small Updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -64,7 +64,7 @@
 
                                 <!-- Desktop Logo -->
                                 <div class="logo col-4 col-sm-4 col-md-2 col-lg-2 text-center">
-                                    <a href="/"><img src="{{ public_path().'/images/logo/artemirate-logo.png' }}" alt="Artemirate" title="Artemirate" /></a>
+                                    <a href="/"><img src="{{ public_path().'/images/logo/artemirate-logo.png' }}" alt="Artemirate" title="Artemirate" width="70%"/></a>
                                 </div>
                                 <!-- End Desktop Logo -->
 
@@ -204,7 +204,7 @@
                             <div class="row">
                                 <!-- Custom block -->
                                 <div class="col-12 col-sm-12 col-md-3 col-lg-3 custom-block">
-                                    <div class="footer-logo"><img src="{{ public_path().'/images/logo/artemirate-logo.png' }}" alt="Artemirate" title="Artemirate" width="130" /></div>
+                                    <div class="footer-logo"><img src="{{ public_path().'/images/logo/artemirate-logo.png' }}" alt="Artemirate" title="Artemirate" /></div>
                                     <div class="text mb-2 mb-md-0 mt-3">Style, elegance and sophistication. Everything on your table. Artemirate is connected with premium international tableware brands. We cater to hotels, restaurants, cafes, or catering companies and offer you personalized services.</div>
                                 </div>
                                 <!-- End Custom block -->
@@ -234,7 +234,7 @@
                                         </li>
                                         <li class="email">
                                             <i class="icon an an-envelope"></i>
-                                            <p><a href="mailto:sales@yousite.com">artemirateindia@gmail.com</a></p>
+                                            <p><a href="mailto:sales@yousite.com">info@artemirate.com</a></p>
                                         </li>
                                     </ul>
                                 </div>
